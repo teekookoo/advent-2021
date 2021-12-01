9cd4d6baa7d6ec318820d238251f13a384c07d36
Tarmo Kivioja <tarmo.kivioja@gmail.com>
2021-12-01 15:10:52 +0200

    Refined Day01
    - No need to compute windowd sum
    - Makes more sense to use =<< instead of >>= with point free style
    - Gave increasedCount a more general type signature

--- app/Day01.hs
+++ app/Day01.hs
@@ -6,19 +6,12 @@ input :: IO [Int]
 input = map read . lines <$> readFile "app/inputs/input01"
 
 solve1 :: IO ()
-solve1 = input >>= print . increasedCount
+solve1 = print . increasedCount 1 =<< input
 
 solve2 :: IO ()
-solve2 = input >>= print . increasedCount . sliding sum 3
+solve2 = print . increasedCount 3 =<< input
 
-increasedCount :: [Int] -> Int
-increasedCount xs = foldr f 0 . zip xs $ drop 1 xs
-  where
-    f (x, y) c
-      | x < y     = c + 1
-      | otherwise = c
-
-sliding :: ([a] -> b) -> Int -> [a] -> [b]
-sliding f n xs = map (f . take n) . take m $ tails xs
-  where
-    m = length xs - n + 1
+increasedCount :: (Ord a, Integral b) => Int -> [a] -> b
+increasedCount offset xs = foldr f 0 . zip xs $ drop offset xs
+  where f (x, y) c | x < y     = c + 1
+                   | otherwise = c
