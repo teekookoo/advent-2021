42db15a7930145fb3e960fe9d9f39b28f9790e6b
Tarmo Kivioja <tarmo.kivioja@gmail.com>
2021-12-01 10:48:35 +0200

    Skeleton in place, ready for Day 1

---
+++ app/DayXX.hs
@@ -0,0 +1,7 @@
+module DayXX (solve1, solve2) where
+
+solve1 :: IO ()
+solve1 = putStrLn "Solution 1"
+
+solve2 :: IO ()
+solve2 = putStrLn "Solution 2"  
---
+++ app/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Day01
+
+main :: IO ()
+main = do
+  putStr "Task 1: "
+  solve1
+  putStr "Task 2: "
+  solve2
