303e88117fb26656545a65c74b3ce7be1aefe3bc
Tarmo Kivioja <tarmo.kivioja@gmail.com>
2021-12-09 14:21:31 +0200

    Day 09 done

--- app/Day09.hs
+++ app/Day09.hs
@@ -1,7 +1,96 @@
 module Day09 (solve1, solve2) where
 
+import           Control.Arrow ((&&&))
+import           Control.Monad ((<=<))
+import           Data.Char (digitToInt)
+import           Data.IntSet (IntSet)
+import qualified Data.IntSet as S
+import           Data.Maybe (mapMaybe)
+import           Data.Ord (comparing)
+import           Data.Vector.Unboxed (Vector, (!), (!?))
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Algorithms.Intro as V
+
+data Heightmap = HM (Vector Height) Int Int -- Numbers nx ny
+type Point = (Int, Int)
+type Basin = IntSet
+type Height = Int
+type Index = Int
+
+testInput :: IO Heightmap
+testInput = pure $ parseInput s
+  where s = "2199943210\n3987894921\n9856789892\n8767896789\n9899965678\n"
+
+input :: IO Heightmap
+input = parseInput <$> readFile "app/inputs/input09"
+
+parseInput :: String -> Heightmap
+parseInput s = HM (V.fromList $ concat nums) nx ny
+  where
+    nums = map (map digitToInt) $ lines s
+    nx = length $ head nums
+    ny = length nums
+
 solve1 :: IO ()
-solve1 = putStrLn "Solution 1"
+solve1 = do
+  -- hm <- testInput
+  hm@(HM v _ _) <- input
+  print . V.sum . V.mapMaybe (fmap (1 +) .  get hm) $ lowPoints hm
 
 solve2 :: IO ()
-solve2 = putStrLn "Solution 2"  
+solve2 = do
+  -- hm <- testInput
+  hm <- input
+  print . V.product . greatest 3 . V.map (basinSize hm) $ lowPoints hm
+  where
+    greatest n = V.take n . V.modify (\v -> V.selectBy (comparing negate) v n)
+    basinSize hm = S.size . basin hm
+
+-- Logic
+
+get :: Heightmap -> Point -> Maybe Height
+get hm@(HM v _ _) = (v !?) . p2i hm <=< validate hm
+
+validate :: Heightmap -> Point -> Maybe Point
+validate (HM v nx ny) (x, y)
+  | 0 <= x && x < nx && 0 <= y && y < ny = Just (x, y)
+  | otherwise                            = Nothing
+
+neighborPoints :: Point -> [Point]
+neighborPoints (x, y) = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
+
+neighbors :: Heightmap -> Point -> [Height]
+neighbors hm = mapMaybe (get hm) . neighborPoints
+
+lowPoints :: Heightmap -> Vector Point
+lowPoints hm@(HM v nx ny) = V.imapMaybe lowPoint v
+  where
+    lowPoint i x
+      | all (x <) . neighbors hm $ i2p hm i = Just $ i2p hm i
+      | otherwise                           = Nothing
+
+
+i2p :: Heightmap -> Index -> Point
+i2p (HM _ nx _) = (`mod` nx) &&& (`div` nx)
+
+p2i :: Heightmap -> Point -> Index
+p2i (HM _ nx _) (x, y) = y*nx + x
+
+basin :: Heightmap -> Point -> IntSet
+basin hm p = go [p] S.empty S.empty
+  where
+    go [] _ b = b
+    go (p:ps) s b
+      | seen      = go ps  s  b
+      | inBasin   = go ps' s' b'
+      | otherwise = go ps  s' b
+      where
+        seen = i `S.member` s
+        inBasin = case get hm p of Nothing -> False
+                                   Just 9  -> False
+                                   _       -> True
+        ps' = neighborPoints p ++ ps
+        s'  = S.insert i s
+        b'  = S.insert i b
+        i   = p2i hm p
+        
